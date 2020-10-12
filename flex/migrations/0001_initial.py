28a61c254853c5f1d9c751d9c385cbd30d20206c
Cami Gomez <camigomez35@gmail.com>
2020-10-12 16:12:09 -0500

    Add testimonials model

--- flex/migrations/0001_initial.py
+++ flex/migrations/0001_initial.py
@@ -9,19 +9,29 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('wagtailcore', '0052_pagelogentry'),
+        ("wagtailcore", "0052_pagelogentry"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='FlexPage',
+            name="FlexPage",
             fields=[
-                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
+                (
+                    "page_ptr",
+                    models.OneToOneField(
+                        auto_created=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        parent_link=True,
+                        primary_key=True,
+                        serialize=False,
+                        to="wagtailcore.page",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Flex (misc) page',
-                'verbose_name_plural': 'Flex (misc) pages',
+                "verbose_name": "Flex (misc) page",
+                "verbose_name_plural": "Flex (misc) pages",
             },
-            bases=('wagtailcore.page',),
+            bases=("wagtailcore.page",),
         ),
     ]
